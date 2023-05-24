8ff8249e02a51d74cc55e009dd2a2c0835c5c7c4
Daniel Esteban Jiménez Morales <djimenezmo3@uniminuto.edu.co>
2023-05-24 18:48:10 -0500

    add Openweather service

--- app/Services/Openweather/Openweather.php
+++ app/Services/Openweather/Openweather.php
@@ -2,15 +2,16 @@
 
 namespace App\Services\Openweather;
 
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client as GuzzleClient;
 
 class Openweather{
 
     public static function getHumidityFromOpenwether( $lng, $lat )
     {
+        $client = new GuzzleClient();
         $bearer = env('OPENWEATHER_ID');
         $link = 'https://api.openweathermap.org/data/3.0/onecall&lng='.$lng.'&lat='.$lat.'&appid='.$bearer;
-        $response = Http::post($link);
+        $response = GuzzleClient::request('GET', $link);
         $bodyresponcs = $response->getBody();
         $result = json_decode($bodyresponcs);
         return $result;
