8ed2583f92efde645b9be89494cb7476f7c0eabc
Daniel Esteban Jiménez Morales <djimenezmo3@uniminuto.edu.co>
2023-05-26 13:13:33 -0500

    change app name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                Browser travel solutions
                 </div>
 
                 <div class="links">
